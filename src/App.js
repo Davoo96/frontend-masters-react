f161dc9e0fb44ef4001e30354452251a7f57c2cd
Davoo96 <davizi96@hotmail.com>
2022-01-13 19:27:43 -0300

    Complete project from start to finish of the Introduction to React course

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import { StrictMode, useState } from "react";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
